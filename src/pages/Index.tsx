338ad879a8c8908bf0682ae2d348e1836b9a5622
H27films <hamzar.128@gmail.com>
2026-03-01 00:57:25 +0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -117,7 +117,7 @@ const Index = () => {
 
       <NewProductPanel open={newProductOpen} onClose={() => setNewProductOpen(false)} rate={store.rate} onAdd={store.addNewProduct} />
       <FullListPanel open={fullListOpen} onClose={() => setFullListOpen(false)} headers={store.fullListHeaders} data={store.fullListData} onImport={store.importFullList} onUpdate={store.updateFullListProduct} onClear={store.clearFullListProduct} rate={store.rate} />
-      <OrderListPanel open={orderListOpen} onClose={() => setOrderListOpen(false)} data={store.data} overrideCNY={store.overrideCNY} overrideQty={store.overrideQty} rate={store.rate} />
+      <OrderListPanel open={orderListOpen} onClose={() => setOrderListOpen(false)} data={store.data} overrideCNY={store.overrideCNY} overrideQty={store.overrideQty} rate={store.rate} onSelectProduct={(row) => { handleSelectProduct(row); setOrderListOpen(false); }} />
     </div>
   );
 };
