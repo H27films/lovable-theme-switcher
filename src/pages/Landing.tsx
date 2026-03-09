df91a9248280091993cc027c5d1507d7c45c90d1
H27Films <hamzar.128@gmail.com>
2026-03-09 09:08:08 +0800

    update from cursor

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -256,7 +256,7 @@ export default function Landing() {
           <style>{`
             @keyframes underlinePulse {
               0%   { width: 30px; }
-              50%  { width: 120px; }
+              50%  { width: 2500px; }
               100% { width: 30px; }
             }
           `}</style>
