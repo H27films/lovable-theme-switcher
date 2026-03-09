acf3a43351c097417fda2d0fc8d7bbf665e1ff2a
H27films <hamzar.128@gmail.com>
2026-03-09 11:25:18 +0800

    Fix: remove missing Landing2 import and route from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
-import Landing2 from "./pages/Landing2";
 import Landing3 from "./pages/Landing3";
 import Index from "./pages/Index";
 import Stock from "./pages/Stock";
@@ -22,7 +21,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/landing2" element={<Landing2 />} />
           <Route path="/landing3" element={<Landing3 />} />
           <Route path="/prices" element={<Index />} />
           <Route path="/stock" element={<Stock />} />
