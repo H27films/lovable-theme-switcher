ff268b7af7be6216180345fd0875d31e52b1aebb
H27films <hamzar.128@gmail.com>
2026-03-12 21:57:18 +0800

    Remove redundant Landing3 page and route

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
-import Landing3 from "./pages/Landing3";
 import LandingSimple from "./pages/LandingSimple";
 import Index from "./pages/Index";
 import Stock from "./pages/Stock";
@@ -27,7 +26,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/landing3" element={<Landing3 />} />
           <Route path="/prices" element={<Index />} />
           <Route path="/office/mobile" element={<IndexPhone />} />
           {/* ── Simple / Boss routes ── */}
