e4a655cb01fce9009216d7c4739a47691a5446f2
H27films <hamzar.128@gmail.com>
2026-08-18 14:16:13 +0800

    Remove legacy desktop pages, keep Simple flow; tidy sub-landing nav and landing IPAD toggle

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingSimple from "./pages/LandingSimple";
-import Office from "./pages/Office";
-import Boudoir from "./pages/Boudoir";
-import BoudoirPhone from "./pages/BoudoirPhone";
-import NurYadi from "./pages/NurYadi";
-import Chic from "./pages/Chic";
-import ChicPhone from "./pages/ChicPhone";
-import NurYadiPhone from "./pages/NurYadiPhone";
-import OfficePhone from "./pages/OfficePhone";
 import SubLandingSimple from "./pages/SubLandingSimple";
 import BoudoirSimple from "./pages/BoudoirSimple";
 import ChicSimple from "./pages/ChicSimple";
@@ -33,8 +25,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<SafeAreaTop><LandingSimple /></SafeAreaTop>} />
-          <Route path="/prices" element={<Office />} />
-          <Route path="/office/mobile" element={<OfficePhone />} />
           {/* ── Simple / Boss routes (all wrapped in TabletScaler + SafeAreaTop for iOS notch / status bar) ── */}
           <Route path="/simple" element={<SafeAreaTop><LandingSimple /></SafeAreaTop>} />
           <Route path="/simple/office" element={<SafeAreaTop><TabletScaler><SubLandingSimple /></TabletScaler></SafeAreaTop>} />
@@ -44,12 +34,6 @@ const App = () => (
           <Route path="/simple/branch-office" element={<SafeAreaTop><TabletScaler><OfficeSimple /></TabletScaler></SafeAreaTop>} />
           <Route path="/simple/order" element={<SafeAreaTop><TabletScaler><OrderSimple /></TabletScaler></SafeAreaTop>} />
           <Route path="/simple/search" element={<SafeAreaTop><TabletScaler><SearchSimple /></TabletScaler></SafeAreaTop>} />
-          <Route path="/stock" element={<Boudoir />} />
-          <Route path="/stock/mobile" element={<BoudoirPhone />} />
-          <Route path="/stocknuryadi" element={<NurYadi />} />
-          <Route path="/stocknuryadi/mobile" element={<NurYadiPhone />} />
-          <Route path="/stockchicnailspa" element={<Chic />} />
-          <Route path="/stockchicnailspa/mobile" element={<ChicPhone />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
