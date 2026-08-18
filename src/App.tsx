7098d121664a97714ccbc3fdb4798e81b72152b3
H27films <hamzar.128@gmail.com>
2026-08-18 12:52:48 +0800

    Make /simple landing the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
 import LandingSimple from "./pages/LandingSimple";
 import Office from "./pages/Office";
 import Boudoir from "./pages/Boudoir";
@@ -33,7 +32,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Landing />} />
+          <Route path="/" element={<SafeAreaTop><LandingSimple /></SafeAreaTop>} />
           <Route path="/prices" element={<Office />} />
           <Route path="/office/mobile" element={<OfficePhone />} />
           {/* ── Simple / Boss routes (all wrapped in TabletScaler + SafeAreaTop for iOS notch / status bar) ── */}
